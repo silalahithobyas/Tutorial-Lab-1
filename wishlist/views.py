66fe3dff3b683599b09b63e12ed6e9be7fb7e337
thobyasmuda <silalahithobyas@gmail.com>
2022-09-15 13:38:36 +0700

    Bikin fungsi

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,7 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from wishlist.models import BarangWishlist
 
 def show_wishlist(request):
@@ -10,3 +13,18 @@ context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Thobyas'
 }
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+
